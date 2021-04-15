99386130a74bb7d13e824e02959d16a79ebcc1e2
Marvin Hülsmann <mrntv64@gmail.com>
2021-04-15 22:23:01 +0200

    adding some new statistic system, fixing some padding bugs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <div class="home">
+    <div class="pt-5 home">
 
-      <br class="no-move"/>
-      <br class="no-move"/>
 
       <b-img rounded="circle" src="../assets/logo.png" class="img"></b-img>
 
@@ -28,6 +26,20 @@
         <p class="my-4 text-dark"><strong>Die App ist noch nicht verfügbar komme bald wieder!</strong></p>
       </b-modal>
 
+
+      <div class="pt-2 yellow-blass-wait">
+        <div class="pb-lg-5 container">
+
+          <h1 class="pt-1 display-title-strong text-white">Entwicklungsstand</h1>
+          <p class="normal-text-secondary text-white">
+            Die App ist kurz vor der Veröffentlichung, sie benötigt schätzungsweise noch {{mathWorkState}}% für die Fertigstellung der App, sie wurde bereits zu circa {{work.workState}}% entwickelt.
+          </p>
+
+          <b-progress :value="work.workState" :max="100" animated variant="dark"></b-progress>
+
+        </div>
+      </div>
+
     </div>
 
     <br class="no-move img-inv-only-handy"/>
@@ -40,12 +52,9 @@
 
     </div>
 
-    <div class="purple-black-wait">
+    <div class="pb-4 pt-4 purple-black-wait">
 
-      <br class="no-move"/>
-
-
-      <h1 class="display-title-strong text-white">Homy - Sicher und Privat</h1>
+      <h1 class="pt-1 display-title-strong text-white">Homy - Sicher und Privat</h1>
       <p class="normal-text-secondary text-white">
         Homy teilt keine Daten in Datenbanken. Sie bleiben einzig und alleine auf deinem Gerät gespeichert.
       </p>
@@ -54,27 +63,18 @@
       <b-img style="padding-left: 30px" src="../assets/iphone-2.png" class="img img-inv-only-handy"></b-img>
 
       <br class="no-move img-inv-only-handy"/>
-      <br class="no-move"/>
     </div>
-
     <br class="no-move"/>
 
-    <div class="orange-blass-wait">
 
-      <br class="no-move"/>
-
+    <div class="pt-3 pb-3 orange-blass-wait">
 
       <h1 class="display-title-strong text-white">Transparent und eine einfache Bedienung</h1>
       <p class="normal-text-secondary text-white ">
         Wir versuchen den Homy Benutzern eine einfache und strukturierte Benutzeroberfläche zu schaffen.
       </p>
 
-
-
-      <br class="no-move "/>
-
     </div>
-
     <br class="no-move"/>
 
     <div class="container">
@@ -83,7 +83,7 @@
         Behalte die neusten Informationen und den Entwicklungsstand der App immer im Auge.
       </p>
 
-      <b-form @submit.prevent>
+      <b-form @submit.prevent="sendEmail">
         <b-form-group
             id="input-group-1"
             label-for="input-1">
@@ -97,7 +97,6 @@
               style="text-align: center"
               required
           ></b-form-input>
-
         </b-form-group>
 
 
@@ -110,9 +109,7 @@
             </div>
             <div v-else-if="waiting" style="text-align: center">
 
-              <br class="no-move"/>
-
-              <b-skeleton animation="throb" width="85%"></b-skeleton>
+              <b-skeleton class="pt-2" animation="throb" width="85%"></b-skeleton>
               <b-skeleton animation="throb" width="55%"></b-skeleton>
               <b-skeleton animation="throb" width="70%"></b-skeleton>
 
@@ -134,14 +131,12 @@
       </b-form>
     </div>
 
-    <br class="no-move"/>
-    <br class="no-move"/>
 
-    <div class="" style="alignment: bottom">
+    <div class="pt-5" style="alignment: bottom">
 
 
       <p class="smaller-end-text">
-        Die App auf <a target="_blank" style="color: #6868b3" href="https://github.com/marvinhuelsmann/Homy">GitHub</a>
+        Die  <a target="_blank" style="color: #6868b3" href="https://github.com/marvinhuelsmann/Homy">App</a> oder die  <a target="_blank" style="color: #6868b3" href="https://github.com/marvinhuelsmann/Homy-Landing-Page">Landing Page</a> auf GitHub
         ansehen.
       </p>
       <p class="smaller-end-text">© {{ new Date().getFullYear() }}
@@ -152,17 +147,18 @@
         verbreiten :3
       </p>
     </div>
-
   </div>
 
 </template>
 
 <script>
-
 export default {
   name: 'Home',
   data() {
     return {
+      work: {
+        workState: null
+      },
       text: '',
       sendEmail: false,
       error: false,
@@ -173,18 +169,30 @@ export default {
       headerTextVariant: 'light',
     }
   },
+  mounted() {
+    fetch('https://marv.link/workState').then(result => {
+      result.json().then(result => {
+        this.work = result
+      })
+    }).catch(error => {
+      console.error(error)
+    })
+  },
+  computed: {
+    mathWorkState() {
+      const currentValue = this.work.workState - 100;
+      return currentValue + currentValue * -2;
+    }
+  },
   methods: {
     onSubmit() {
-      this.mounted()
-
+      this.sendEmailData()
     },
-    mounted() {
-
+    sendEmailData() {
       if (this.text === '' || !this.text.includes("@")) {
         this.error = true
         return;
       }
-
       this.waiting = true;
 
       fetch('https://marv.link/addMail?mail=' + this.text).then(response => {
@@ -196,7 +204,6 @@ export default {
           }
         })
       }).finally(() => this.waiting = false)
-
     }
   }
 }
